3bc579ddf7f85309924072eb0c6c724b92cf10f6
Sebas15897 <sebastiancontreras15897@gmail.com>
2021-02-12 16:42:13 +0000

    updateConsole

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Import and define class"""
+
+import cmd
+
+promt = "(hbnb)"
+
+def do_quit(self, arg):
+        """Quit command to exit the program\n"""
+        return True
+
+def do_EOF(self, args):
+    """End of File to exit file."""
+    return True
+
+def emptyline(self):
+    """method so it should not execute anything"""
+    pass
